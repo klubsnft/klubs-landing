c786ff3cdccab6dd341bb3ebfbc70f39554472a2
Sim Young Jae <hanul@hanul.me>
2022-06-21 15:52:26 +0900

    dev

---
+++ lib/component/shared/menu/PCMenu.d.ts
@@ -0,0 +1,5 @@
+import { DomNode } from "skydapp-browser";
+export default class PCMenu extends DomNode {
+    constructor();
+}
+//# sourceMappingURL=PCMenu.d.ts.map
---
+++ lib/view/governance/CreateGovernance.d.ts
@@ -0,0 +1,9 @@
+import { View, ViewParams } from "skydapp-common";
+export default class CreateGovernance implements View {
+    private container;
+    constructor();
+    private init;
+    changeParams(params: ViewParams, uri: string): void;
+    close(): void;
+}
+//# sourceMappingURL=CreateGovernance.d.ts.map
